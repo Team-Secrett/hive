6019d4ab701d96d99e3bef09a065dde0e4b739e7
samueldsr99 <samueldsr8@gmail.com>
2021-12-07 16:20:30 -0500

    feat: Add some input parser functions

--- src/environment.pl
+++ src/environment.pl
@@ -1,12 +1,11 @@
-:- module(utils, [
-  step/2,
-  get_player_turn/2,
-  cell_neighbours/4
-]).
+% :- module(utils, [
+%   step/2,
+%   get_player_turn/2,
+%   cell_neighbours/4
+% ]).
 
 
 % Get the 6 neighbours of a cell
-
 cell_neighbours(Col, Diag, NCols, NDiags) :-
   UP is Diag - 1,
   DOWN is Diag + 1,
@@ -16,17 +15,13 @@ cell_neighbours(Col, Diag, NCols, NDiags) :-
   NDiags = [UP, UP, Diag, DOWN, DOWN, Diag].
 
 % Check if 2 cells are adjacent
-adjacent(C1, D1, C2, D2) :-
-  write("C1:"), write(C1),
-  write("C2:"), write(C2),
-  write("C3:"), write(C3),
-  write("C4:"), write(C4),
+is_adjacent(C1, D1, C2, D2) :-
   C1 = C2, D1 is D2 - 1; % up
   C1 = C2, D1 is D2 + 1; % down
-  C1 is C2 - 1, D1 is D2 - 1; % diag right up
+  C1 is C2 - 1, D1 = D2; % diag right up
   C1 is C2 - 1, D1 is D2 + 1; % diag right down
   C1 is C2 + 1, D1 is D2 - 1; % diag left up
-  C1 is C2 + 1, D1 is D2 + 1. % diag left down
+  C1 is C2 + 1, D1 = D2. % diag left down
 
 % Piece methods
 
@@ -39,32 +34,59 @@ remove_piece(Piece) :- retract(Piece).
 % Get DB pieces
 get_pieces(Pieces) :-
   findall(
-    piece(Class, Color, C, D, Stacked),
-    piece(Class, Color, C, D, Stacked),
+    piece(Class, Color, Id, C, D, Stacked),
+    piece(Class, Color, Id, C, D, Stacked),
     Pieces
   ).
 
 % Get the piece neighbours of a position
 piece_neighbours(C, D, Neighbours) :-
   findall(
-    piece(Class, Color, NC, ND, Stacked),
-    adjacent(NC, ND, C, D),
+    piece(Class, Color, Id, NC, ND, Stacked),
+    (
+      piece(Class, Color, Id, NC, ND, Stacked),
+      is_adjacent(NC, ND, C, D)
+    ),
     Neighbours
   ).
+
+% Move a piece to a new position
+move_piece(position(C, D, S), position(NC, ND, NS)) :-
+  remove_piece(piece(Class, Color, Id, C, D, S)), !,
+  add_piece(piece(Class, Color, Id, NC, ND, NS)).
+
 
 % Get the player turn (1 or 2) based on current Turn number
 get_player_turn(Turn, Ans) :- Ans is 2 - mod(Turn, 2).
 
-% Make a move based on MoveStr code & updates env
-step(MoveStr, Color) :-
-  sub_string(MoveStr, 0, 1, _, Color).
+get_piece_from_str(Str, Piece) :-
+  string_chars(Str, [Color, Class, Id]),
+  atom_number(Id, IId),
+  piece(Class, Color, IId, C, D, S),
+  Piece = piece(Class, Color, IId, C, D, S).
 
+parse_action(ActionStr, Action) :-
+  sub_string(ActionStr, 0, 3, _, Str1),
+  sub_string(ActionStr, _, 3, 0, Str2),
+  sub_string(ActionStr, 3, 2, _, Side),
+
+  get_piece_from_str(Str1, Piece1),
+  get_piece_from_str(Str2, Piece2),
+
+  Action = action(Piece1, Piece2, Side).
 
 % Test methods
 test() :-
-  add_piece(piece('M', 'w', 0, 0, 0)),
-  add_piece(piece('M', 'w', 0, 1, 0)),
-  add_piece(piece('M', 'w', 1, 1, 0)),
-  piece_neighbours(0, 1, Neighbours),
-  write(Neighbours).
+  add_piece(piece('b', 'w', 1, 0, 1, 0)),
+  add_piece(piece('a', 'w', 1, 0, 0, 0)),
+  add_piece(piece('m', 'b', 1, 1, 1, 0)),
+  parse_action("wb1/*wa1", Action),
+  write(Action).
+  % get_piece_from_str("wb1", piece(Class, Color, Id, C, D, S)),
+  % move_piece(position(C, D, S), position(9, 9, 0)),
+  % get_pieces(N),
+  % write(N),
+  % write('\n'),
+  % piece_neighbours(C, D, Ne),
+  % write(Ne).
 
