62e48791f0848eaa9071f168304cced44734491d
samueldsr99 <samueldsr8@gmail.com>
2021-12-07 22:06:02 -0500

    refactor: Change coords system

--- src/environment.pl
+++ src/environment.pl
@@ -6,22 +6,22 @@
 
 
 % Get the 6 neighbours of a cell
-cell_neighbours(Col, Diag, NCols, NDiags) :-
-  UP is Diag - 1,
-  DOWN is Diag + 1,
-  LEFT is Col - 1,
-  RIGHT is Col + 1,
-  NCols = [Col, RIGHT, RIGHT, Col, LEFT, LEFT],
-  NDiags = [UP, UP, Diag, DOWN, DOWN, Diag].
+cell_neighbours(R, D, NR, ND) :-
+  UP is R - 1,
+  DOWN is R + 1,
+  LEFT is D - 1,
+  RIGHT is D + 1,
+  NR = [UP, UP, R, DOWN, DOWN, R],
+  ND = [D, RIGHT, RIGHT, D, LEFT, LEFT].
 
 % Check if 2 cells are adjacent
-is_adjacent(C1, D1, C2, D2) :-
-  C1 = C2, D1 is D2 - 1; % up
-  C1 = C2, D1 is D2 + 1; % down
-  C1 is C2 - 1, D1 = D2; % diag right up
-  C1 is C2 - 1, D1 is D2 + 1; % diag right down
-  C1 is C2 + 1, D1 is D2 - 1; % diag left up
-  C1 is C2 + 1, D1 = D2. % diag left down
+is_adjacent(R1, D1, R2, D2) :-
+  R1 = R2, D1 is D2 - 1; % up
+  R1 = R2, D1 is D2 + 1; % down
+  R1 is R2 - 1, D1 = D2; % diag right up
+  R1 is R2 - 1, D1 is D2 + 1; % diag right down
+  R1 is R2 + 1, D1 is D2 - 1; % diag left up
+  R1 is R2 + 1, D1 = D2. % diag left down
 
 % Piece methods
 
@@ -34,27 +34,26 @@ remove_piece(Piece) :- retract(Piece).
 % Get DB pieces
 get_pieces(Pieces) :-
   findall(
-    piece(Class, Color, Id, C, D, Stacked),
-    piece(Class, Color, Id, C, D, Stacked),
+    piece(Class, Color, Id, R, D, Stacked),
+    piece(Class, Color, Id, R, D, Stacked),
     Pieces
   ).
 
 % Get the piece neighbours of a position
-piece_neighbours(C, D, Neighbours) :-
+piece_neighbours(R, D, Neighbours) :-
   findall(
-    piece(Class, Color, Id, NC, ND, Stacked),
+    piece(Class, Color, Id, NR, ND, Stacked),
     (
-      piece(Class, Color, Id, NC, ND, Stacked),
-      is_adjacent(NC, ND, C, D)
+      piece(Class, Color, Id, NR, ND, Stacked),
+      is_adjacent(NR, ND, R, D)
     ),
     Neighbours
   ).
 
 % Move a piece to a new position
-move_piece(position(C, D, S), position(NC, ND, NS)) :-
-  remove_piece(piece(Class, Color, Id, C, D, S)), !,
-  add_piece(piece(Class, Color, Id, NC, ND, NS)).
-
+move_piece(position(R, D, S), position(NR, ND, NS)) :-
+  remove_piece(piece(Class, Color, Id, R, D, S)), !,
+  add_piece(piece(Class, Color, Id, NR, ND, NS)).
 
 % Get the player turn (1 or 2) based on current Turn number
 get_player_turn(Turn, Ans) :- Ans is 2 - mod(Turn, 2).
@@ -62,10 +61,17 @@ get_player_turn(Turn, Ans) :- Ans is 2 - mod(Turn, 2).
 get_piece_from_str(Str, Piece) :-
   string_chars(Str, [Color, Class, Id]),
   atom_number(Id, IId),
-  piece(Class, Color, IId, C, D, S),
-  Piece = piece(Class, Color, IId, C, D, S).
+  piece(Class, Color, IId, R, D, S),
+  Piece = piece(Class, Color, IId, R, D, S).
 
 parse_action(ActionStr, Action) :-
+  string_length(ActionStr, Length),
+  Length = 3,
+  get_piece_from_str(Str1, Piece1),
+  add_piece(Piece1).
+parse_action(ActionStr, Action) :-
+  string_length(ActionStr, Length),
+  Length = 8,
   sub_string(ActionStr, 0, 3, _, Str1),
   sub_string(ActionStr, _, 3, 0, Str2),
   sub_string(ActionStr, 3, 2, _, Side),
@@ -75,18 +81,26 @@ parse_action(ActionStr, Action) :-
 
   Action = action(Piece1, Piece2, Side).
 
+step(action(Piece1, Piece2, Side)) :-
+  1 = 1.
+
 % Test methods
 test() :-
   add_piece(piece('b', 'w', 1, 0, 1, 0)),
   add_piece(piece('a', 'w', 1, 0, 0, 0)),
   add_piece(piece('m', 'b', 1, 1, 1, 0)),
+
   parse_action("wb1/*wa1", Action),
+  % parse_action("wb1", Action),
   write(Action).
+  % move_piece(position(0, 1, S), position(9, 9, 0)),
   % get_piece_from_str("wb1", piece(Class, Color, Id, C, D, S)),
-  % move_piece(position(C, D, S), position(9, 9, 0)),
   % get_pieces(N),
   % write(N),
   % write('\n'),
+  % write(C),
+  % write('\n'),
+  % write(D),
+  % write('\n'),
   % piece_neighbours(C, D, Ne),
   % write(Ne).
-
