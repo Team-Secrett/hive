e5b10f2b3665d4c33137032b4f5055332829aaa6
samueldsr99 <samueldsr8@gmail.com>
2021-12-09 00:00:45 -0500

    feat: Add clear_screen method

--- src/pvp.pl
+++ src/pvp.pl
@@ -20,15 +20,17 @@ turn_info(Turn) :-
   get_pieces(Pieces),
   write_line(Pieces).
 
+clear_screen() :- write('\33\[2J').
+
 pvp_loop(Turn) :-
+  clear_screen(),
   turn_info(Turn),
   write_line('Type your move'),
   read_line_to_string(user_input, MoveStr),
   write_list(['> You typed: ', MoveStr, '\n']),
   parse_action(MoveStr, Action),
   write_list(["Action: ", Action, '\n']),
-  step(Action),
-
+  (step(Action) ; true),
   NextTurn is Turn + 1,
   pvp_loop(NextTurn).
 
